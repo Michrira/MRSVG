8a53d58e865d1ed1a55cfd0ab55c124bb420bb94
Michrira <Michaelchristianrader@gmail.com>
2023-04-05 16:29:05 -0700

    created shapes.js / index.js

---
+++ develop/lib/shapes.js
@@ -0,0 +1,11 @@
+class Triangle {
+    // implement the triangle class here
+}
+
+class Circle {
+    // implement the circle class here
+}
+
+class Square {
+    // implement the square class here
+}
