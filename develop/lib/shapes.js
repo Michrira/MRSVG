0ae61aa3beb4508f2294daecd7f2715be6e0b083
Michrira <Michaelchristianrader@gmail.com>
2023-04-05 17:33:27 -0700

    tests pass!

--- develop/lib/shapes.js
+++ develop/lib/shapes.js
@@ -1,43 +1,36 @@
-export class Shape {
-  constructor() {
-    if (new.target === Shape) {
-      throw new TypeError("Cannot construct Shape instances directly.");
-    }
+class Shapes {
+  constructor(color){
+      this.color = color
   }
-
-  setColor(color) {
-    this.color = color;
+  setColor (color) {
+      this.color = color 
   }
-
-  render() {
-    throw new Error("Method not implemented.");
+}
+class Circle extends Shapes {
+  constructor (color) {
+      super(color)
+  }
+  render (){
+      return `<circle cx="150" cy="100" r="80" stroke="${this.color}" fill="${this.color}" />`
+  }
+}
+class Triangle extends Shapes {
+  constructor (color) {
+      super (color)
+  }
+  render (){
+      return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`
+  }
+}
+class Square extends Shapes {
+  constructor (color) {
+      super (color)
+  }
+  render () {
+      return  `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`
   }
 }
 
-export class Triangle extends Shape {
-  render() {
-    return `<polygon points="100,50 50,150 150,150" style="fill:${this.color};" />`;
-  }
-}
-
-export class Circle extends Shape {
-  constructor(radius) {
-    super();
-    this.radius = radius;
-  }
-
-  render() {
-    return `<circle cx="150" cy="150" r="${this.radius}" style="fill:${this.color};" />`;
-  }
-}
-
-export class Square extends Shape {
-  constructor(sideLength) {
-    super();
-    this.sideLength = sideLength;
-  }
-
-  render() {
-    return `<rect x="50" y="50" width="${this.sideLength}" height="${this.sideLength}" style="fill:${this.color};" />`;
-  }
+module.exports = {
+ Circle, Triangle, Square
 }
